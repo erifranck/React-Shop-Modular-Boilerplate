ed5024f311b2cfce768aa82a8b35e144451f01d1
erifranck <erifranckjose@gmail.com>
2017-12-28 18:20:03 -0500

    export wrapper with redux

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,12 @@
 import { App, Home } from 'main/components'
+import {connect} from 'react-redux'
+import {onHover, onBlur} from 'main/actions'
 
 export const AppPage = App
-export const HomePage = Home
+export const HomePage = connect((state) => ( {
+  ishover: state.main.ishover,
+  selected: state.main.selected,
+}), {
+  onHover,
+  onBlur
+})( Home )
