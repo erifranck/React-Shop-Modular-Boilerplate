a9e86276f0e97d696c58e93c0456b4e0dab1cc49
erifranck <erifranckjose@gmail.com>
2017-12-28 19:50:19 -0500

    change styles and insert text

--- src/commons/components/Cards.jsx
+++ src/commons/components/Cards.jsx
@@ -5,6 +5,7 @@ export const Card = () => (
   <CardStyles>
     <div className='card-wrapper'>
       <div className='card-container'>
+        hola soy erifranck soy desarrollador frontend desde hace 3 años, amante del arte y de javascript en general
       </div>
     </div>
   </CardStyles>
--- src/commons/styles/Card.js
+++ src/commons/styles/Card.js
@@ -3,7 +3,6 @@ import variables from 'commons/styles/styleVariables'
 
 export const Card = glamorous.div(
   {
-    padding: '20px',
     position: 'absolute',
     width: '100%',
     left: '0',
@@ -13,6 +12,7 @@ export const Card = glamorous.div(
       '& .card-container': {
         display: 'block',
         margin: 'auto',
+        padding: '20px',
         boxShadow: ' -47px 19px 191px -67px rgba(0,0,0,0.75)',
         backgroundColor: variables.bgcolor,
         width: '100%',
