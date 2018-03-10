422dac36f94a3801f2bcfb70b0a0661ffba1b92f
erifranck <erifranjose@hotmail.com>
2018-03-10 15:30:39 -0500

    create theme file

---
+++ src/root/theme.js
@@ -0,0 +1,20 @@
+import colors from 'commons/styles/colorpalette'
+
+const theme = {
+  fonts: {
+    family: 'Hervetica',
+    color: colors.blue
+  },
+  colors: {
+    primary: colors.sky,
+    secondary: colors.pink,
+    inverse: colors.white,
+    neutral: colors.gray
+  },
+  main: {
+    bgcolor: colors.white
+
+  }
+}
+
+export default theme
