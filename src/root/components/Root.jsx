95c715353b5c9e50ac7a1b0c97a914a23656bcd7
erifranck <erifranjose@hotmail.com>
2018-03-10 15:29:47 -0500

    insert theme provider

--- src/root/components/Root.jsx
+++ src/root/components/Root.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { HashRouter } from 'react-router-dom'
 import { renderRoutes } from 'react-router-config'
+import {ThemeProvider} from 'glamorous'
+import theme from 'root/theme'
 
 // custom import
 import routes from 'root/routes'
 
 export const Root = ({ store }) => (
   <Provider store={store}>
-    <HashRouter>{renderRoutes(routes)}</HashRouter>
+    <ThemeProvider theme={theme}>
+        <HashRouter>{renderRoutes(routes)}</HashRouter>
+    </ThemeProvider>
   </Provider>
 )
