34d0b666023e5c793c09c7254487b6b917100625
erifranck <erifranjose@hotmail.com>
2018-03-10 15:30:22 -0500

    remove unnecesary elements

--- src/main/components/Home.jsx
+++ src/main/components/Home.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
-import {Layout, FlipCard, Card} from 'commons/components'
+import {Layout} from 'commons/components'
 import {Home as HomeStyles} from 'main/styles'
-import {Grid, Row, Col} from 'react-flexbox-grid'
 import profile from 'commons/images/perfil.jpg'
 
 export class Home extends Component {
@@ -23,29 +22,7 @@ export class Home extends Component {
     return(
       <Layout>
         <HomeStyles >
-          <div className='profile-wrapper'>
-            <Grid>
-              <Row>
-                <Col md={8}>
-                  <h2>
-                    Frontend Developer / Ilustrator / Entrepreneur
-                  </h2>
-                </Col>
-                <Col md={4}>
-                  <Row end='xs'>
-                    <FlipCard />
-                  </Row>
-                </Col>
-              </Row>
-            </Grid>
-          </div>
         </HomeStyles>
-        <Card
-          position='absolute'
-          width='50%'
-          top='30%'
-          left='15%'
-        />
       </Layout>
     )
   }
