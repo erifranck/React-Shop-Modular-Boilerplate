6b3142a57aa91b02050430e794bb671526772d6a
erifranck <erifranjose@hotmail.com>
2018-03-11 01:56:43 -0500

    create paper styles

---
+++ src/commons/styles/Paper.js
@@ -0,0 +1,14 @@
+import glamorous from 'glamorous'
+
+const boxShadow = [
+  'rgba(0, 0, 0, 0.12) 0px 1px 6px, rgba(0, 0, 0, 0.12) 0px 1px 4px',
+  'rgba(0, 0, 0, 0.16) 0px 3px 10px, rgba(0, 0, 0, 0.23) 0px 3px 10px',
+  'rgba(0, 0, 0, 0.19) 0px 10px 30px, rgba(0, 0, 0, 0.23) 0px 6px 10px',
+  'rgba(0, 0, 0, 0.25) 0px 14px 45px, rgba(0, 0, 0, 0.22) 0px 10px 18px',
+  'rgba(0, 0, 0, 0.3) 0px 19px 60px, rgba(0, 0, 0, 0.22) 0px 15px 20px'
+]
+
+export const Paper = glamorous.div(({theme, level = 1}) => ({
+  boxShadow: boxShadow[level],
+  background: theme.colors.inverse
+}))
