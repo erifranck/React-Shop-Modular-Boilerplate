a0fa4a841a7e9a9b105d4aa20489b034eb95d65d
erifranck <erifranjose@hotmail.com>
2018-03-12 08:42:52 -0500

    add shopping car ç

--- src/main/components/Home.jsx
+++ src/main/components/Home.jsx
@@ -1,12 +1,14 @@
 import React, {Component} from 'react'
 import {Layout} from 'commons/components'
 import {Home as HomeStyles} from 'main/styles'
+import shopCar from 'commons/images/car.png'
 
 export class Home extends Component {
   render () {
     return (
       <Layout>
         <HomeStyles>
+          <img src={shopCar} alt="shopCar" />
         </HomeStyles>
       </Layout>
     )
--- src/main/styles/Home.js
+++ src/main/styles/Home.js
@@ -3,5 +3,11 @@ import glamorous from 'glamorous'
 export const Home = glamorous.div(({theme}) => ({
   width: '400px',
   height: '400px',
-  border: `1px solid ${theme.colors.primary}`
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  '& img': {
+    width: '250px',
+    height: '250px'
+  }
 }))
