5c31e4c907cd106eb219e4e4c2dec738a57f8880
erifranck <erifranjose@hotmail.com>
2018-03-11 21:29:01 -0500

    add form reducer

--- src/root/reducers.js
+++ src/root/reducers.js
@@ -3,10 +3,12 @@ import { combineReducers } from 'redux'
 // custom import
 import { reducer as main } from 'main/reducers'
 import { reducer as product } from 'products/reducers'
+import { reducer as formReducer } from 'redux-form'
 
 const rootReducer = combineReducers({
   main,
-  product
+  product,
+  form: formReducer
 })
 
 export default rootReducer
