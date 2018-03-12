3615c86a9ccc164c7492596538e174eed723bc54
erifranck <erifranjose@hotmail.com>
2018-03-11 22:55:01 -0500

    add card wrapper styles

--- src/products/styles/ProductList.js
+++ src/products/styles/ProductList.js
@@ -2,5 +2,9 @@ import glamorous from 'glamorous'
 
 export const ProductList = glamorous.div(() => ({
   height: '500px',
-  width: '80%'
+  width: '80%',
+  '& .card-wrapper': {
+    display: 'inline-block',
+    marginLeft: '20px'
+  }
 }))
