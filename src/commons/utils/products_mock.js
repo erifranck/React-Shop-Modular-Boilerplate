0fba9b62fc6ea9bd1fe995bf07d58241bf6447dd
erifranck <erifranjose@hotmail.com>
2018-03-11 22:54:05 -0500

    create new initial products

--- src/commons/utils/products_mock.js
+++ src/commons/utils/products_mock.js
@@ -1,9 +1,27 @@
+import react from 'commons/images/reactjs.png'
+import angular from 'commons/images/angular.png'
+import vue from 'commons/images/vuejs.png'
+
 export default [
   {
     id: 1,
-    name: "",
-    price: "",
-    store: "",
-    image: ""
-  }
+    name: 'React App Services',
+    description: 'Make your application with reactjs',
+    price: '30 USD per hours',
+    image: react
+  },
+  {
+    id: 2,
+    name: 'Angular App Services',
+    description: 'Make your application with angular',
+    price: '30 USD per hours',
+    image: angular
+  },
+  {
+    id: 3,
+    name: 'Vue App Services',
+    description: 'Make your application with Vue',
+    price: '30 USD per hours',
+    image: vue
+  },
 ]
