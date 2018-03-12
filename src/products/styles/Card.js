33a97aecb962495140067d0543f2ce63d0c81dde
erifranck <erifranjose@hotmail.com>
2018-03-11 22:54:27 -0500

    add new styles

--- src/products/styles/Card.js
+++ src/products/styles/Card.js
@@ -1,5 +1,16 @@
 import glamorous from 'glamorous'
 
 export const Card = glamorous.div(({theme}) => ({
-  height: theme.main.card.height
+  height: theme.main.card.height,
+  width: theme.main.card.width,
+  padding: '20px',
+  '& .card-header': {
+    height: '100px',
+    '& img': {
+      width: '100px',
+      height: '100px',
+      display: 'block',
+      margin: 'auto'
+    }
+  }
 }))
