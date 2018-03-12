bdf58a195ca43abecdac262aa1d8252a18e91737
erifranck <erifranjose@hotmail.com>
2018-03-12 08:45:42 -0500

    change stateless component to eslint component

--- src/products/components/Form.jsx
+++ src/products/components/Form.jsx
@@ -1,27 +1,49 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Layout} from 'commons/components'
 import {Form as FormStyles} from 'products/styles'
 import { Field, reduxForm } from 'redux-form'  // ES6
 import {required} from 'commons/utils/fieldLevelValidationForm'
 import {RenderSimpleField} from 'commons/components/Fields'
 
-const FormComponent = () => (
-  <Layout>
-    <FormStyles>
-      <form action='' >
-        <Field
-          name='firstname'
-          type='text'
-          placeholder='Joe'
-          validate={[required]}
-          required
-          component={RenderSimpleField}
-          label='FIRST NAME'
+class FormComponent extends Component {
+  constructor () {
+    super()
+    this.sendForm = this.sendForm.bind(this)
+  }
+
+  sendForm () {
+
+  }
+
+  render () {
+    return (
+      <Layout>
+        <FormStyles>
+          <form action='' >
+            <Field
+              name='firstname'
+              type='text'
+              placeholder='Joe'
+              validate={[required]}
+              required
+              component={RenderSimpleField}
+              label='FIRST NAME'
         />
-      </form>
-    </FormStyles>
-  </Layout>
-)
+            <Field
+              name='firstname'
+              type='text'
+              placeholder='Joe'
+              validate={[required]}
+              required
+              component={RenderSimpleField}
+              label='FIRST NAME'
+        />
+          </form>
+        </FormStyles>
+      </Layout>
+    )
+  }
+}
 export const Form = reduxForm({
   form: 'addproduct'
 })(FormComponent)
